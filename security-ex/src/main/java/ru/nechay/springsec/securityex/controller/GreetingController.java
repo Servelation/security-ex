49120e90cebe4ec81730a1461d3135c2319c10c8
Ne4ay <A-lex00-7@yandex.ua>
2021-05-14 22:03:51 +0300

    Created and connect db

---
+++ security-ex/src/main/java/ru/nechay/springsec/securityex/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package ru.nechay.springsec.securityex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+
+	
+	@GetMapping("/greeting")
+	public String greeting(Model model) {
+		
+		return "greeting/greet";
+	}
+}
